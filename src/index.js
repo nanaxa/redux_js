162cf38a48b40a6e46f730cc3d1ff136e25c2adc
nanaxa <pavpildan@gmail.com>
2021-06-30 23:05:21 +0700

    create actions, and edit index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import {createStore} from "redux";
 import {rootReducer} from "./redux/rootReducer";
+import {decrement, increment} from "./redux/actions";
 
 let counter = document.querySelector(".counter");
 let subBtn = document.querySelector(".sub");
@@ -11,12 +12,12 @@ let asyncBtn = document.querySelector(".async");
 const store = createStore(rootReducer, 0);
 
 addBtn.addEventListener('click', ()=> {
-        store.dispatch({type: 'INCREMENT'})
+        store.dispatch( increment() )
     }
 )
 
 subBtn.addEventListener('click',() => {
-    store.dispatch({type: 'DECREMENT'})
+    store.dispatch( decrement() )
 })
 
 
